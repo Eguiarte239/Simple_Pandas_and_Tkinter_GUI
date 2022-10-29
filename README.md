012012dce0ba4bfa813d27352ba7faa49525a2e8
Abraham Eguiarte <abraham.lopez5085@alumnos.udg.mx>
2022-10-28 19:21:52 -0500

    Código mejorado, ahora se pueden obtener estadísticas y se modificó ligeramente el archivo usuarios.txt

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Simple GUI to retrieve data from CSV using pandas and Tkinter
 As its name says, this is a pretty simple GUI created with Tkinter with which you'll be able to retrieve data from a CSV file and display it in a list box.
 You can filter by age and occupation. If you decide to filter it by age then you'll be able to add another filter, selecting one out of three age ranges to display.
+In addition to this, you'll now be able to get the amount of records with the same occupation
+or age, depending on your filter.
--- usuarios.py
+++ usuarios.py
@@ -1,101 +1,94 @@
 import tkinter as tk
 from tkinter import END, ttk
-from tkinter.colorchooser import *
 from tkinter.ttk import *
 import pandas as pd
+import ctypes
+
+ctypes.windll.shcore.SetProcessDpiAwareness(True)
 
 df = pd.read_csv('usuarios.txt')
 
 def on_field_change():
-    if filtradoPrincipal.get() == "Edad":
-        filtroSec.set('Filtrado por edad')
-        filtroSec.place(x = 250, y = 15)
-        btnSec.place(x = 272, y = 45, width=100, height=25)
-    else:
-        filtroSec.place_forget()
-        btnSec.place_forget()
- 
-def filtrarPrincipal():
-    on_field_change()
-    if filtradoPrincipal.get() == "Edad":
-        listbox.delete(0,END)
-        shownData = list(df[['id','Nombre', 'Edad']].values)
-        for data in shownData:
-            listbox.insert(END, '---------------------------------------------')
-            listbox.insert(END, ("ID: "+str(data[0])), ("Nombre: "+str(data[1])), ("Edad: "+str(data[2])))
+    if comboboxMainFilter.get() == "Edad":
+        secFilter.set('Filtrado por edad')
+        secFilter.place(x = 340, y = 15)
+        btnSec.place(x = 380, y = 45, width=100, height=25)
     
-    elif filtradoPrincipal.get() == "Ocupacion":
-        listbox.delete(0,END)
-        shownData = list(df[['id','Nombre', 'Ocupacion']].values)
-        for data in shownData:
-            listbox.insert(END, '--------------------------------------------------------')
-            listbox.insert(END, ("ID: "+str(data[0])), ("Nombre: "+str(data[1])), ("Ocupación: "+str(data[2])))
+    else:
+        secFilter.place_forget()
+        btnSec.place_forget()
+    
+def functionMainFilter(columna):
+    listboxData.delete(0,END)
+    shownData = list(df[['id','Nombre', columna]].values)
+    for data in shownData:
+        listboxData.insert(END, '---------------------------------------------')
+        listboxData.insert(END, ("ID: "+str(data[0])), ("Nombre: "+str(data[1])), (columna+": "+str(data[2])))
 
+def functionSecondaryFilter(min, max):
+    listboxData.delete(0,END)
+    secondaryDf = df[df['Edad'].between(min, max)]
+    shownData = list(secondaryDf[['id', 'Nombre', 'Edad']].values)
+    for data in shownData:
+        listboxData.insert(END, '---------------------------------------------')
+        listboxData.insert(END, ("ID: "+str(data[0])), ("Nombre: "+str(data[1])), ("Edad: "+str(data[2])))
 
-def filtrarSecundario(filtrado):
+def mainFilter():
+    on_field_change()
+    if comboboxMainFilter.get() == "Edad":
+        functionMainFilter('Edad')
+    
+    elif comboboxMainFilter.get() == "Ocupación":
+        functionMainFilter('Ocupación')
+
+def secondaryFilter(filtrado):
     if filtrado.get() == "15 - 44":
-        listbox.delete(0,END)
-        secondaryDf = df[df['Edad'].between(15, 44)]
-        shownData = list(secondaryDf[['id', 'Nombre', 'Edad']].values)
-        for data in shownData:
-            listbox.insert(END, '---------------------------------------------')
-            listbox.insert(END, ("ID: "+str(data[0])), ("Nombre: "+str(data[1])), ("Edad: "+str(data[2])))
+        functionSecondaryFilter(15, 44)
     
     elif filtrado.get() == "45 - 84":
-        listbox.delete(0,END)
-        secondaryDf = df[df['Edad'].between(45, 84)]
-        shownData = list(secondaryDf[['id', 'Nombre', 'Edad']].values)
-        for data in shownData:
-            listbox.insert(END, '---------------------------------------------')
-            listbox.insert(END, ("ID: "+str(data[0])), ("Nombre: "+str(data[1])), ("Edad: "+str(data[2])))
+        functionSecondaryFilter(45, 84)
     
     elif filtrado.get() == "85 - 90":
-        listbox.delete(0,END)
-        secondaryDf = df[df['Edad'].between(85, 90)]
-        shownData = list(secondaryDf[['id', 'Nombre', 'Edad']].values)
-        for data in shownData:
-            listbox.insert(END, '---------------------------------------------')
-            listbox.insert(END, ("ID: "+str(data[0])), ("Nombre: "+str(data[1])), ("Edad: "+str(data[2])))
+        functionSecondaryFilter(85, 90)
 
-def filtrarEstadisticas():
+def statistics():
     on_field_change()
-    if filtradoPrincipal.get() == "Edad":
-        listbox.delete(0,END)
-        shownData = dict(df['Edad'].value_counts(ascending=True))
+    if comboboxMainFilter.get() == "Edad":
+        listboxData.delete(0,END)
+        shownData = dict(df['Edad'].value_counts(sort=True))
         for key, value in shownData.items():
-            listbox.insert(END, '--------------------------------------------------------')
-            listbox.insert(END, ("Edad: "+str(key)), ("Cantidad de personas con esta edad: "+str(value)))
+            listboxData.insert(END, '--------------------------------------------------------')
+            listboxData.insert(END, ("Edad: "+str(key)), ("Cantidad de personas con esta edad: "+str(value)), ("Porcentaje de personas con esta edad: "+str(value*100/1000)+"%"))
     
-    elif filtradoPrincipal.get() == "Ocupacion":
-        listbox.delete(0,END)
-        shownData = dict(df['Ocupacion'].value_counts(ascending=True))
+    elif comboboxMainFilter.get() == "Ocupación":
+        listboxData.delete(0,END)
+        shownData = dict(df['Ocupación'].value_counts(ascending=True))
         for key, value in shownData.items():
-            listbox.insert(END, '--------------------------------------------------------')
-            listbox.insert(END, ("Ocupación: "+str(key)), ("Cantidad de personas con esta ocupación: "+str(value)))
+            listboxData.insert(END, '--------------------------------------------------------')
+            listboxData.insert(END, ("Ocupación: "+str(key)), ("Cantidad de personas con esta ocupación: "+str(value)), ("Porcentaje de personas con esta ocupación: "+str(value*100/1000)+"%"))
 
-lst = ['Edad', 'Ocupacion']
+filterList = ['Edad', 'Ocupación']
 
-lst2 = ['15 - 44', '45 - 84', '85 - 90']
+secFilterList = ['15 - 44', '45 - 84', '85 - 90']
 mw = tk.Tk()
-filtroSec = filtradoSecundario = ttk.Combobox(mw,value=lst2)
-btnSec = btnFiltradoSecundario = tk.Button(text='Filtrar edad', command=lambda: filtrarSecundario(filtradoSecundario))
+secFilter = comboboxSecondaryFilter = ttk.Combobox(mw,value=secFilterList)
+btnSec = btnSecondaryFilter = tk.Button(text='Filtrar edad', command=lambda: secondaryFilter(comboboxSecondaryFilter))
 mw.title('Usuarios')
-mw.geometry("500x500") 
+mw.geometry("600x600") 
 mw.resizable(0, 0)
  
-filtradoPrincipal = ttk.Combobox(mw,value=lst)
-filtradoPrincipal.set('Filtrado')
-filtradoPrincipal.place(x = 78, y = 15)
+comboboxMainFilter = ttk.Combobox(mw,value=filterList)
+comboboxMainFilter.set('Filtrado')
+comboboxMainFilter.place(x = 78, y = 15)
 
-btnFiltradoPrincipal = tk.Button(text='Filtrar', command=filtrarPrincipal)
-btnFiltradoPrincipal.place(x = 100, y = 45, width=100, height=25)
+btnMainFilter = tk.Button(text='Filtrar', command=mainFilter)
+btnMainFilter.place(x = 120, y = 45, width=100, height=25)
 
-btnFiltradoEstadisticas = tk.Button(text='Obtener estadísticas', command=filtrarEstadisticas)
-btnFiltradoEstadisticas.place(x = 190, y = 420, width=120, height=30)
+btnStatistics = tk.Button(text='Obtener estadísticas', command=statistics)
+btnStatistics.place(x = 240, y = 520, width=160, height=30)
 
-listbox = tk.Listbox(mw)
-listbox.grid(padx = 50, pady= 75)
-listbox.config(width=65, height=20)
-
+listboxData = tk.Listbox(mw)
+listboxData.grid(padx = 50, pady= 75)
+listboxData.config(width=65, height=20)
  
 mw.mainloop()
